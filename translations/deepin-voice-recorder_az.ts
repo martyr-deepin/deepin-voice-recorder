936aa62bed77b5a0b43f0e4851afa9429d23692e
transifex <transifex@linuxdeepin.com>
2018-04-04 06:35:27 +0000

    auto sync po files from transifex
    Change-Id: I476b8c4f51d7f4e04ce60661ae0b3684b06fcc82

--- translations/deepin-voice-recorder_az.ts
+++ translations/deepin-voice-recorder_az.ts
@@ -14,7 +14,7 @@
     <message>
         <location filename="../src/file_view.cpp" line="63"/>
         <source>Move to trash</source>
-        <translation type="unfinished"/>
+        <translation>Zibil qutusuna daşıyın</translation>
     </message>
 </context>
 <context>
--- translations/deepin-voice-recorder_bn.ts
+++ translations/deepin-voice-recorder_bn.ts
@@ -1,20 +1,20 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="bn" version="2.1">
 <context>
     <name>FileView</name>
     <message>
         <location filename="../src/file_view.cpp" line="59"/>
         <source>Rename</source>
-        <translation>重命名</translation>
+        <translation>পূনরায় নাম দিন</translation>
     </message>
     <message>
         <location filename="../src/file_view.cpp" line="61"/>
         <source>Display in file manager</source>
-        <translation>在文件管理器中显示</translation>
+        <translation>ফাইল ম্যানেজারে দেখান</translation>
     </message>
     <message>
         <location filename="../src/file_view.cpp" line="63"/>
         <source>Move to trash</source>
-        <translation>移动到回收站</translation>
+        <translation>ট্র্যাশে সরান</translation>
     </message>
 </context>
 <context>
@@ -22,42 +22,42 @@
     <message>
         <location filename="../src/main_window.cpp" line="53"/>
         <source>New recording</source>
-        <translation>新录音</translation>
+        <translation>নতুন রেকর্ডিং</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="55"/>
         <source>Open saved directory</source>
-        <translation>打开保存目录</translation>
+        <translation>যেখানে সংরক্ষণ করা হয়েছে সেই পথ খুলুন</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="57"/>
         <source>About</source>
-        <translation>关于</translation>
+        <translation>সম্পর্কে</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="59"/>
         <source>Help</source>
-        <translation>帮助</translation>
+        <translation>সাহায্য</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="61"/>
         <source>Exit</source>
-        <translation>退出</translation>
+        <translation>বের হয়ে যান</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="168"/>
         <source>Deepin Voice Recorder is a beautiful and easy to use voice recording application with simple design. It supports visual recording, recording playback, recording list management and other functions.</source>
-        <translation>深度录音是一款设计简洁、美观易用的录音软件。它支持可视化录音、录音回放、录音列表管理等功能。</translation>
+        <translation>ডিপিন ভয়েস রেকর্ডার সুন্দর সহজে ব্যবহার যোগ্য এবং সাধারণ ডিজাইনের একটি ভয়েস রেকর্ডার এপ্লিকেশন। এটি ভিজ্যুয়াল রেকর্ডিং, রেকর্ডিং প্লেব্যাক, রেকর্ডিং তালিকা ব্যবস্থাপনা এবং অন্যান্য ফাংশন সমর্থন করে।</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="180"/>
         <source>Deepin Voice Recorder</source>
-        <translation>深度录音</translation>
+        <translation>ডিপিন ভয়েস রেকর্ডার</translation>
     </message>
     <message>
         <location filename="../src/main_window.cpp" line="181"/>
         <source>Version</source>
-        <translation>版本</translation>
+        <translation>ভার্সন</translation>
     </message>
 </context>
 <context>
@@ -65,7 +65,7 @@
     <message>
         <location filename="../src/main.cpp" line="45"/>
         <source>Deepin Voice Recorder</source>
-        <translation>深度录音</translation>
+        <translation>ডিপিন ভয়েস রেকর্ডার</translation>
     </message>
 </context>
 <context>
@@ -74,7 +74,7 @@
         <location filename="../src/record_page.cpp" line="55"/>
         <location filename="../src/record_page.cpp" line="219"/>
         <source>New recording</source>
-        <translation>新录音</translation>
+        <translation>নতুন রেকর্ডিং</translation>
     </message>
 </context>
 <context>
@@ -82,7 +82,7 @@
     <message>
         <location filename="../src/utils.cpp" line="136"/>
         <source>Recording</source>
-        <translation>录音</translation>
+        <translation>রেকর্ড করা হচ্ছে</translation>
     </message>
 </context>
 </TS>
