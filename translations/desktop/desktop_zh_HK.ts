85655adbf467c78b9b1519af1734d2c2410b6240
rekols <rekols@foxmail.com>
2018-11-23 09:59:30 +0800

    update desktop
    Change-Id: Ia2d8782c866d47fe051de61296e63b67b4353f16

---
+++ translations/desktop/desktop_zh_HK.ts
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_HK"><context><name>desktop</name><message><location filename="Desktop Entry]Name" line="0"/><source>Deepin Voice Recorder</source><translation>深度錄音</translation></message><message><location filename="Desktop Entry]Comment" line="0"/><source>Record voice</source><translation>聲音錄製工具</translation></message><message><location filename="Desktop Entry]GenericName" line="0"/><source>Voice Recorder</source><translation type="unfinished"/></message></context></TS>
